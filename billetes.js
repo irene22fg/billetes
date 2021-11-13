7af0acc75ae234c85b33f2ad066fc02b23121d75
irene22fg <irenefiguereo22@gmail.com>
2021-11-13 21:38:27 +0000

    fin

---
+++ billetes.js
@@ -0,0 +1,38 @@
+export const data = {
+    imagenes: [{
+            "code": 1,
+            "value":5,
+            "image": "billete5.jpg"
+        },
+        {
+            "code": 2,
+            "value":10,
+            "image": "billete10.jpg"
+        },
+        {
+            "code": 3,
+            "value":20,
+            "image": "billete20.jpg"
+        },
+        {
+            "code": 4,
+            "value":50,
+            "image": "billete50.jpg"
+        },
+        {
+            "code": 5,
+            "value":100,
+            "image": "billete100.jpg"
+        },
+        {
+            "code": 6,
+            "value":200,
+            "image": "billete200.jpg"
+        },
+        {
+            "code": 7,
+            "value":500,
+            "image": "billete500.jpg"
+        },
+    ]
+}
---
+++ main.js
@@ -0,0 +1,49 @@
+import {data} from "/billetes.js";
+const imagenes = data.imagenes;
+
+if(localStorage.getItem("total") == null){
+    localStorage.setItem("total", JSON.stringify(0));
+}
+else
+document.getElementById("total").firstElementChild.textContent = localStorage.getItem("total") + '€';
+
+let divContainer = document.getElementById("billetes");
+
+(function(){
+    imagenes.forEach(element => {mostrarImagenes(element)});
+    document.getElementById("restart").addEventListener('click', reset);
+})()
+
+function mostrarImagenes(foto){
+    let img = crearNodo("img", "", [], [{name:"src", value:"./img/" + foto.image}, {name:"id", value:foto.value}, {name:"width", value:"250"}, {name:"height", value:"130"}]);
+    img.addEventListener('click', mostrarTotal);
+    divContainer.appendChild(img);
+}
+
+function mostrarTotal(event){
+    let valor = JSON.parse(localStorage.getItem("total"));
+    valor += parseInt(event.target.id);
+    localStorage.removeItem("total");
+    localStorage.setItem("total",JSON.stringify(valor));
+    document.getElementById("total").firstElementChild.textContent = localStorage.getItem("total") + '€';
+}
+
+function reset(){
+    localStorage.removeItem("total");
+    localStorage.setItem("total", JSON.stringify(0));
+    document.getElementById("total").firstElementChild.textContent = localStorage.getItem("total") + '€';
+}
+
+function crearNodo(tipo, texto, clases, atributos) {     //función CREAR NODO
+    let nodo = document.createElement(tipo);
+    if (texto != "" && texto != null) {
+        nodo.appendChild(document.createTextNode(texto));
+    }
+    if (clases.length > 0) {
+        clases.forEach(clase => nodo.classList.add(clase));
+    }
+    if (atributos.length > 0) {
+        atributos.forEach(atributo => nodo.setAttribute(atributo.name, atributo.value));
+    }
+    return nodo;
+}
